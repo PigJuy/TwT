cd7a1e1c5051bf792d840d9fc66928c857a53bf7
PigJuy <114330108+PigJuy@users.noreply.github.com>
2022-09-25 16:02:17 +0800

    Update main.py

--- main.py
+++ main.py
@@ -36,7 +36,8 @@ def get_weather(region):
         'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) '
                       'AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.0.0 Safari/537.36'
     }
-    key = config["weather_key"]
+#     key = config["weather_key"]
+    key = c28f0da3a0fe4d6ba1777fd97f043a77
     region_url = "https://geoapi.qweather.com/v2/city/lookup?location={}&key={}".format(region, key)
     response = get(region_url, headers=headers).json()
     if response["code"] == "404":
